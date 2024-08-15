1c91c6f14330ef4dfde3c228fbab005f9052a423
Fadzaa <fadza20@gmail.com>
2024-08-15 22:52:01 +0700

    add stackview widget list story

--- app/src/main/java/com/example/story_dicoding/view/stackwidget/StackRemoteViewsFactory.kt
+++ app/src/main/java/com/example/story_dicoding/view/stackwidget/StackRemoteViewsFactory.kt
@@ -2,36 +2,34 @@ package com.example.story_dicoding.view.stackwidget
 
 import android.content.Context
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.util.Log
 import android.widget.RemoteViews
 import android.widget.RemoteViewsService
 import androidx.core.os.bundleOf
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import com.bumptech.glide.Glide
 import com.example.story_dicoding.R
 import com.example.story_dicoding.model.preferences.SettingPreferences
-import com.example.story_dicoding.model.preferences.dataStore
 import com.example.story_dicoding.model.remote.ApiConfig
 import com.example.story_dicoding.model.remote.response.AllStoryResponse
 import com.example.story_dicoding.model.remote.response.Story
-import com.example.story_dicoding.model.repository.StoryRepository
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
 
-internal class StackRemoteViewsFactory(private val mContext: Context): RemoteViewsService.RemoteViewsFactory  {
+internal class StackRemoteViewsFactory(private val mContext: Context, settingPreferences: SettingPreferences): RemoteViewsService.RemoteViewsFactory  {
     private val mWidgetItems = ArrayList<Story>()
-    private val apiService = ApiConfig.getApiService(SettingPreferences.getInstance(mContext.dataStore))
+    private val apiService = ApiConfig.getApiService(settingPreferences)
 
     override fun onCreate() {
 
     }
 
     override fun onDataSetChanged() {
-        apiService.getAllStory(1, 1, 0).enqueue(
+        val latch = CountDownLatch(1)
+
+        apiService.getAllStory(1, 10, 0).enqueue(
             object : Callback<AllStoryResponse> {
                 override fun onResponse(
                     call: Call<AllStoryResponse>,
@@ -41,16 +39,30 @@ internal class StackRemoteViewsFactory(private val mContext: Context): RemoteVie
                         response.body()?.let {
                             for (story in it.listStory) {
                                 mWidgetItems.add(story)
+                                Log.d("StackRemoteViewsFactory", "onResponse: ${story.description}")
                             }
                         }
+
+                        Log.d("StackRemoteViewsFactory", "onResponse: ${mWidgetItems.size}")
                     }
+
+                    latch.countDown()
                 }
 
                 override fun onFailure(call: Call<AllStoryResponse>, t: Throwable) {
                     Log.e("StackRemoteViewsFactory", "onFailure: ${t.message.toString()}")
+                    latch.countDown()
                 }
             }
         )
+
+        try {
+            latch.await(5, TimeUnit.SECONDS)  // wait max 5 seconds
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+
+        Log.d("StackRemoteViewsFactory", "onDataSetChanged: ${mWidgetItems.size}")
     }
 
     override fun onDestroy() {
@@ -69,40 +81,27 @@ internal class StackRemoteViewsFactory(private val mContext: Context): RemoteVie
         val fillInIntent = Intent()
         fillInIntent.putExtras(extras)
         rv.setOnClickFillInIntent(R.id.image_view, fillInIntent)
-        return rv
+//        rv.setTextViewText(R.id.tv_widget, mWidgetItems[position].description)
+        val imageBitmap = Glide.with(mContext)
+            .asBitmap()
+            .load(mWidgetItems[position].photoUrl)
+            .submit()
+            .get()
 
+        rv.setImageViewBitmap(R.id.image_view, imageBitmap)
+
+
+
+        return rv
     }
 
-    override fun getLoadingView(): RemoteViews? = null
+    override fun getLoadingView(): RemoteViews {
+        return RemoteViews(mContext.packageName, R.layout.loading_view)
+    }
 
     override fun getViewTypeCount(): Int = 1
 
     override fun getItemId(i: Int): Long = 0
 
     override fun hasStableIds(): Boolean = false
-
-//    fun getAllStory(): AllStoryResponse {
-//        var allStoryResponse: AllStoryResponse? = null
-//
-//        apiService.getAllStory(1, 1, 0).enqueue(
-//            object : Callback<AllStoryResponse> {
-//                override fun onResponse(
-//                    call: Call<AllStoryResponse>,
-//                    response: Response<AllStoryResponse>
-//                ) {
-//                    if (response.isSuccessful) {
-//                        response.body()?.let {
-//                            allStoryResponse = it
-//                        }
-//                    }
-//                }
-//
-//                override fun onFailure(call: Call<AllStoryResponse>, t: Throwable) {
-////                    Log.e(, "onFailure: ${t.message.toString()}")
-//                }
-//            }
-//        )
-//
-//        return allStoryResponse!!
-//    }
 }
